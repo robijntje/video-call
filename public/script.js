3403b3d6b788c03690ab9499f684fde57e4ac49c
Robin Hein Anna Matheus Friesen III <44802556+robijntje@users.noreply.github.com>
2021-05-27 13:29:44 +0200

    Let socket.io listen to new user script
    Added code to let socket.io listen to the new user script and log the user id in the console.

--- public/script.js
+++ public/script.js
@@ -1,2 +1,6 @@
 const socket = io('/') //socket verbind met hoofdmap (root)
 socket.emit('join-room', ROOM_ID, 10)
+
+socket.on('new-user', userId => {
+    console.log('Gebruiker verbonden > ' + userId)
+})
