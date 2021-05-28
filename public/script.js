a11e60e150d5d0dbae981f46bb215397508b1155
robijntje <44802556+robijntje@users.noreply.github.com>
2021-05-28 14:48:16 +0200

    Added first code for video of user
    Added code to create an element that shows the video feed of the user, and mutes the user (to make sure the user doesn't hear himself through the application).

--- public/script.js
+++ public/script.js
@@ -1,9 +1,15 @@
 const socket = io('/') //socket verbind met hoofdmap (root)
 
+const videoGrid = document.getElementById('video-grid')
+
 const newPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
 })
+
+const userVideo = document.createElement('video')
+userVideo.muted = true
+
 newPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
 })
