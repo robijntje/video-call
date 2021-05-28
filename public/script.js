74af5718b14dea440d6438c3602b3632f38c6bbe
robijntje <44802556+robijntje@users.noreply.github.com>
2021-05-28 14:40:46 +0200

    Added PeerJS
    Added PeerJS to create peer-to-peer connections with WebRTC and create random user IDs.

--- public/script.js
+++ public/script.js
@@ -1,4 +1,10 @@
 const socket = io('/') //socket verbind met hoofdmap (root)
+
+const newPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+
 socket.emit('join-room', ROOM_ID, 10)
 
 socket.on('new-user', userId => {
