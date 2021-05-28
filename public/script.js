629551852e077b607289d405111dd06777c83012
robijntje <44802556+robijntje@users.noreply.github.com>
2021-05-28 14:57:20 +0200

    Added user video stream
    Added the code to handle the camera stream, and added the stream to the video grid.

--- public/script.js
+++ public/script.js
@@ -10,6 +10,13 @@ const newPeer = new Peer(undefined, {
 const userVideo = document.createElement('video')
 userVideo.muted = true
 
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+addVideoStream(userVideo, stream)
+})
+
 newPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
 })
@@ -17,3 +24,11 @@ newPeer.on('open', id => {
 socket.on('new-user', userId => {
     console.log('Gebruiker verbonden > ' + userId)
 })
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
